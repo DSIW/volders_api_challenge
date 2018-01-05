5e86f649495469ed350401a7285f14ab7a814c61
DSIW <dsiw@dsiw-it.de>
2018-01-05 13:57:19 +0100

    Add POST /contracts route for controller action

--- spec/integration/routes_spec.rb
+++ spec/integration/routes_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe Api.routes do
     expect(route.verb).to   eq 'POST'
     expect(route.params).to eq({})
   end
+
+  it 'generates "/contracts"' do
+    actual = described_class.path(:contracts)
+    expect(actual).to eq '/contracts'
+  end
+
+  it 'recognizes "POST /contracts"' do
+    env = Rack::MockRequest.env_for('/contracts', method: 'POST')
+    route = described_class.recognize(env)
+
+    expect(route).to be_routable
+
+    expect(route.path).to   eq '/contracts'
+    expect(route.verb).to   eq 'POST'
+    expect(route.params).to eq({})
+  end
 end
