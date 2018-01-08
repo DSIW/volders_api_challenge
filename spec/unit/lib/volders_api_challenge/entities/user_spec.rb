42dbb013d71d2400d618cf4b000279b3aa5b0b57
DSIW <dsiw@dsiw-it.de>
2018-01-08 18:04:00 +0100

    Improve tests for user entity

--- spec/unit/lib/volders_api_challenge/entities/user_spec.rb
+++ spec/unit/lib/volders_api_challenge/entities/user_spec.rb
@@ -1,5 +1,25 @@
 describe User do
+  let(:user) { described_class.new(full_name: 'Max Mustermann', email: 'max@mustermann.de', password: 'password') }
+
   it 'extends from Hanami::Entity' do
-    expect(User.new).to be_a Hanami::Entity
+    expect(described_class.new).to be_a Hanami::Entity
+  end
+
+  describe "#full_name" do
+    it 'returns the full name' do
+      expect(user.full_name).to eq 'Max Mustermann'
+    end
+  end
+
+  describe "#email" do
+    it 'returns the email' do
+      expect(user.email).to eq 'max@mustermann.de'
+    end
+  end
+
+  describe "#password" do
+    it 'returns the password' do
+      expect(user.password).to eq 'password'
+    end
   end
 end
